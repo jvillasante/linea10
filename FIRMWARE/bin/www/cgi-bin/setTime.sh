273b04856e582c09fed30f3c165baca361a1d7c8
Julio C. Villasante <jvillasantegomez@gmail.com>
2014-11-06 11:59:30 -0300

    si NTP esta corriendo, el usuario no puede cambiar la fecha

--- FIRMWARE/bin/www/cgi-bin/setTime.sh
+++ FIRMWARE/bin/www/cgi-bin/setTime.sh
@@ -10,45 +10,47 @@ echo '<title>Interfaz para el cambio de la fecha y hora del dispositivo </title>
 echo '</head>'
 echo '<body>'
 
-MMddhhmm[[cc]yy]
-echo "<form method=GET action=\"${SCRIPT}\">"\
-  '<table nowrap>'\
-  '<tr><td>Formato de la fecha a especificar (MMddhhmmccyy) </TD></tr>'\
-  '<tr><td>MM: Mes (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
-  '<tr><td>dd: D&iacute;a (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
-  '<tr><td>hh: Hora (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
-  '<tr><td>mm: Minutos (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
-  '<tr><td>cc: Primeros dos d&iacute;gitos del a&ntilde;o </TD></tr>'\
-  '<tr><td>yy: Ultimos dos d&iacute;gitos del a&ntilde;o </TD></tr>'\
-  '<tr><td>Fecha y Hora Nueva:</TD><TD><input type="text" name="m_date" size=14></td></tr>'\
-  '</table>'
-
-echo '<br><input type="submit" value="Guardar"></form>'
-
-# Make sure we have been invoked properly.
-
-if [ "$REQUEST_METHOD" != "GET" ]; then
-  echo "<hr>Error en Script:"\
-    "<br>el metodo de solicitud nos es GET"\
-    "<br>Revise la declaracion de su formulario METHOD=\"GET\".<hr>"
-  exit 1
-fi
-
-# If no search arguments, exit gracefully now.
-
-if [ -z "$QUERY_STRING" ]; then
-  exit 0
+if [ -f /usr/local/bin/Resources/ntp_is_running ]; then
+  echo "NTP actualiza la fecha y hora autom&aacute;ticamente. NTP esta corriendo en este sistema.<br>"
+  echo "No es necesario actualizar la fecha y hora."
 else
-  # No looping this time, just extract the data you are looking for with sed:
-  xDate=`echo "$QUERY_STRING" | sed -n 's/^.*m_date=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
+  echo "<form method=GET action=\"${SCRIPT}\">"\
+    '<table nowrap>'\
+    '<tr><td>Formato de la fecha a especificar (MMddhhmmccyy) </TD></tr>'\
+    '<tr><td>MM: Mes (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
+    '<tr><td>dd: D&iacute;a (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
+    '<tr><td>hh: Hora (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
+    '<tr><td>mm: Minutos (num&eacute;rico de dos d&iacute;gitos) </TD></tr>'\
+    '<tr><td>cc: Primeros dos d&iacute;gitos del a&ntilde;o </TD></tr>'\
+    '<tr><td>yy: Ultimos dos d&iacute;gitos del a&ntilde;o </TD></tr>'\
+    '<tr><td>Fecha y Hora Nueva:</TD><TD><input type="text" name="m_date" size=14></td></tr>'\
+    '</table>'
+  echo '<br><input type="submit" value="Guardar"></form>'
+  
+  # Make sure we have been invoked properly.
+  if [ "$REQUEST_METHOD" != "GET" ]; then
+    echo "<hr>Error en Script:"\
+      "<br>el metodo de solicitud nos es GET"\
+      "<br>Revise la declaracion de su formulario METHOD=\"GET\".<hr>"
+    exit 1
+  fi
 
-  echo "SE CAMBIARA LA FECHA Y HORA DEL EQUIPO COMO SIGUE:"
-  echo "Fecha y Hora Nueva: " $xDate
-  echo '<br>'
-  echo '<br>'
+  # If no search arguments, exit gracefully now.
+  if [ -z "$QUERY_STRING" ]; then
+    exit 0
+  else
+    # No looping this time, just extract the data you are looking for with sed:
+    xDate=`echo "$QUERY_STRING" | sed -n 's/^.*m_date=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 
-  # ./SetTimeKairos $xDate
-  date $xDate
+    echo "SE CAMBIARA LA FECHA Y HORA DEL EQUIPO COMO SIGUE<br>"
+    echo "Fecha y Hora (MMddhhmmccyy): " $xDate
+    echo '<br>'
+    echo '<br>'
+
+    date $xDate
+    hwclock --systohc
+  fi
 fi
+
 echo '</body>'
 echo '</html>'
